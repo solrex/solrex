892fd70aa1107aaa84faf1909dd924836ae11108
Wenbo Yang <solrex@users.noreply.github.com>
2025-03-13 11:19:24 +0800

    Update fp8_dynamic_quant.py

--- snippets/fp8_dynamic_quant.py
+++ snippets/fp8_dynamic_quant.py
@@ -7,14 +7,13 @@ from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
 from llmcompressor import oneshot
 from llmcompressor.modifiers.quantization import QuantizationModifier
 
-
-def apply_fp8_quant_to_llm(model_dir, output_dir):
+def apply_fp8_quant_to_llm(model_dir, output_dir, device_map):
     if output_dir == None:
         output_dir = model_dir.split("/")[-1] + "-FP8-Dynamic"
 
     # Load model.
     model = AutoModelForCausalLM.from_pretrained(
-        model_dir, device_map="auto", torch_dtype="auto"
+        model_dir, device_map=device_map, torch_dtype="auto"
     )
     tokenizer = AutoTokenizer.from_pretrained(model_dir)
     
@@ -30,7 +29,7 @@ def apply_fp8_quant_to_llm(model_dir, output_dir):
         add_generation_prompt=True
     )
     print("========== SAMPLE BEFORE QUANT ==============")
-    inputs = tokenizer([text], return_tensors="pt").to('cuda')
+    inputs = tokenizer([text], return_tensors="pt").to(device_map)
     streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
     model.generate(
         input_ids=inputs['input_ids'],
@@ -55,17 +54,16 @@ def apply_fp8_quant_to_llm(model_dir, output_dir):
 
     # Load output model
     model = AutoModelForCausalLM.from_pretrained(
-        output_dir, device_map="auto", torch_dtype="auto"
+        model_dir, device_map=device_map, torch_dtype="auto"
     )
-    tokenizer = AutoTokenizer.from_pretrained(output_dir)
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
     
     # Confirm generations of the quantized model look sane.
     print("========== SAMPLE AFTER QUANT ==============")
-    inputs = tokenizer([text], return_tensors="pt").to('cuda')
-    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+    inputs = tokenizer([text], return_tensors="pt").to(device_map)
     model.generate(
-        input_ids=inputs['input_ids'].to('cuda'),
-        attention_mask=inputs['attention_mask'].to('cuda'),
+        input_ids=inputs['input_ids'],
+        attention_mask=inputs['attention_mask'],
         streamer=streamer,
         max_new_tokens=400)
     print("==========================================")
@@ -74,5 +72,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Do FP8-Dynamic quant for model')
     parser.add_argument('model_dir', nargs='?', default='.', help='Model directory (default: $PWD)')
     parser.add_argument('--output_dir', '-o', type=str, default=None, help='Output directory (default: $model_dir-FP8-Dynamic')
+    parser.add_argument('--device_map', '-d', type=str, default='cuda:0', help='device_map, can be: auto, cuda:0, cuda:0,1')
     args = parser.parse_args()
-    apply_fp8_quant_to_llm(args.model_dir, args.output_dir)
+    apply_fp8_quant_to_llm(args.model_dir, args.output_dir, args.device_map)
