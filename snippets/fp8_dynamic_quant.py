b0797b5b4e3947b18fd4a8a53033af859ac2f26d
Wenbo Yang <solrex@users.noreply.github.com>
2025-03-13 10:23:26 +0800

    Add FP8 Quant script.

---
+++ snippets/fp8_dynamic_quant.py
@@ -0,0 +1,72 @@
+#!/bin/env python3
+import sys
+import argparse
+
+from transformers import AutoTokenizer, AutoModelForCausalLM, TextStreamer
+
+from llmcompressor import oneshot
+from llmcompressor.modifiers.quantization import QuantizationModifier
+
+
+def apply_fp8_quant_to_llm(model_dir, output_dir):
+    if output_dir == None:
+        output_dir = model_dir.split("/")[-1] + "-FP8-Dynamic"
+
+    # Load model.
+    model = AutoModelForCausalLM.from_pretrained(
+        model_dir, device_map="auto", torch_dtype="auto"
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_dir)
+    
+    # Confirm generations of the quantized model look sane.
+    prompt = "请输出《滕王阁序》全文"
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": prompt}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    print("========== SAMPLE BEFORE QUANT ==============")
+    inputs = tokenizer([text], return_tensors="pt").to('cuda')
+    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+    model.generate(
+        input_ids=inputs['input_ids'],
+        attention_mask=inputs['attention_mask'],
+        streamer=streamer,
+        max_new_tokens=800)
+    print("==========================================")
+    
+    # Configure the quantization algorithm and scheme.
+    # In this case, we:
+    #   * quantize the weights to fp8 with per channel via ptq
+    #   * quantize the activations to fp8 with dynamic per token
+    recipe = QuantizationModifier(
+        targets="Linear",
+        scheme="FP8_DYNAMIC",
+        ignore=["lm_head"],
+    )
+    
+    # Apply quantization and save to disk in compressed-tensors format.
+    oneshot(model=model, recipe=recipe, tokenizer=tokenizer, output_dir=output_dir)
+    tokenizer.save_pretrained(output_dir)
+    
+    # Confirm generations of the quantized model look sane.
+    print("========== SAMPLE AFTER QUANT ==============")
+    inputs = tokenizer([text], return_tensors="pt").to('cuda')
+    streamer = TextStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
+    model.generate(
+        input_ids=inputs['input_ids'].to('cuda'),
+        attention_mask=inputs['attention_mask'].to('cuda'),
+        streamer=streamer,
+        max_new_tokens=800)
+    print("==========================================")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Do FP8-Dynamic quant for model')
+    parser.add_argument('model_dir', nargs='?', default='.', help='Model directory (default: $PWD)')
+    parser.add_argument('--output_dir', '-o', type=str, default=None, help='Output directory (default: $model_dir-FP8-Dynamic')
+    args = parser.parse_args()
+    apply_fp8_quant_to_llm(args.model_dir, args.output_dir)
